7245dc256266ce03945637cadf2b8fb959aba279
Jieun <ewqsaz123@hanmail.net>
2022-06-13 16:55:20 +0900

    code commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-
 const dummyList=[
   {
     id: 1,
     author: "jieun",
     content : "hi 1",
     emotion: 1,
-    created_date : new Date("2022-06-08 13:23:24").getTime(),
+    created_date : new Date().getTime(),
   },
   {
     id: 2,
     author: "sol",
     content : "hi 2",
     emotion: 2,
-    created_date : new Date("2022-06-08 13:23:24").getTime(),
+    created_date : new Date().getTime(),
   },
   {
     id: 3,
     author: "juhyun",
     content : "hi 3",
     emotion: 3,
-    created_date : new Date("2022-06-08 13:23:24").getTime(),
+    created_date : new Date().getTime(),
   },
   
 ];
